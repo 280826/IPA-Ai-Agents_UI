ca6f0d9036a0717958cc8441bec76c1aa607b4ab
Deepak-280826 <280826@cognizant.com>
2025-12-04 23:37:17 +0530

    login routes fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://ipaaiagents-fpd9hrbzfvh9dab2.centralus-01.azurewebsites.net/api/endpoints'
+  apiBaseUrl: 'https://ipaaiagents-fpd9hrbzfvh9dab2.centralus-01.azurewebsites.net/api'
 };
