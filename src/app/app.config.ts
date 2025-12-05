5929a563d01b93759b657d6e9c2cbac05037b4c8
Deepak-280826 <280826@cognizant.com>
2025-12-05 15:48:17 +0530

    agent-store page update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, Provider } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-    provideAnimations()
-  ]
+
+    provideHttpClient(withInterceptorsFromDi()), // enable DI interceptors
+    // register the DI interceptor
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } as Provider,
+  ],
 };
